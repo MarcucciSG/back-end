510720979dc7b3626111600c8ac80f1163fc5adc
SantiagoMarcucci <marcuccisantiago8@gmail.com>
2023-08-05 20:25:39 -0300

    creado casi todo falta el ultimo metodo

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-class productManager{
-    
+class productManager {
+  constructor() {
+    this.products;
+    this.autoId = 0;
+  }
 }
+
+addProducts(title, description, price, thumbnail, code, stock);
+{
+  const newProduct = {
+    title,
+    description,
+    price,
+    thumbnail,
+    code,
+    stock,
+    id: this.autoId + 1,
+  };
+
+  const validateCode = this.products.find(
+    (product) => product.code === newProduct.code
+  );
+  if (validateCode) {
+    console.log("Error, el codigo ingresado ya esta en uso");
+  } else {
+    this.products.push(newProduct);
+    this.lastId++;
+  }
+}
+
+getProducts();
+{
+  console.log(this.products);
+}
+
