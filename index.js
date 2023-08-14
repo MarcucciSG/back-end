065e9a69486f1f29fa407469bc5255c0a44c22e8
SantiagoMarcucci <marcuccisantiago8@gmail.com>
2023-08-14 19:24:14 -0300

    desafio 2

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+const fs = require("fs");
+
 class ProductManager {
+  products;
+
   constructor() {
     this.products = [];
-    this.autoId = 0;
+    this.path = "./products.json";
   }
 
-  addProducts(title, description, price, thumbnail, code, stock) {
+  async getProducts() {
+    const data = JSON.parse(await fs.promises.readFile(this.path, "utf-8"));
+    return data;
+  }
+
+  async getId() {
+    let data = await this.getProducts();
+    return data.length + 1;
+  }
+
+  async addProduct(title, description, price, thumbnail, code, stock) {
     const newProduct = {
       title,
       description,
@@ -12,39 +26,95 @@ class ProductManager {
       thumbnail,
       code,
       stock,
-      id: this.autoId + 1,
     };
 
-    const validateCode = this.products.find(
-      (products) => products.code === newProduct.code
-    );
-    if (validateCode) {
-      console.log("Error, el codigo ingresado ya esta en uso");
-    } else {
-      this.products.push(newProduct);
-      this.lastId++;
+    try {
+      if (!fs.existsSync(this.path)) {
+        const emptyList = [];
+        emptyList.push({ ...newProduct, id: await this.getId() });
+
+        await fs.promises.writeFile(
+          this.path,
+          JSON.stringify(emptyList, null, "\t")
+        );
+      } else {
+        const data = await this.getProducts();
+        const repeatCode = data.some((e) => e.code == newProduct.code);
+        repeatCode == true
+          ? console.log("El codigo esta repetido")
+          : data.push({ ...newProduct, id: await this.getId() });
+        await fs.promises.writeFile(
+          this.path,
+          JSON.stringify(data, null, "\t")
+        );
+      }
+    } catch (err) {
+      console.log(err);
     }
   }
 
-  getProducts() {
-    console.log(this.products);
+  async getProductsById(id) {
+    const data = await this.getProducts();
+    let productFind = data.find((e) => e.id == id);
+    return productFind === undefined ? console.log("Not found") : productFind;
   }
 
-  getProductsById(id) {
-    let searchId = this.products.find ((i) => i.id == id);
-    if (searchId === undefined) {
-      console.log("Not Found");
-    } else {
-      return searchId;
-    }
+  async deleteProduct(id) {
+    const data = await this.getProducts();
+    let i = data.findIndex((e) => e.id === id);
+    data.splice(i, 1);
+    await fs.promises.writeFile(this.path, JSON.stringify(data));
+  }
+
+  async updateProducts(id, product) {
+    let data = await this.getProducts();
+    let i = data.findIndex((e) => e.id === id);
+    product.id = id;
+    data.splice(i, 1, product);
+    await fs.promises.writeFile(this.path, JSON.stringify(data));
   }
 }
 
-const productManager =  new ProductManager()
+const funcionAsync = async () => {
+  const productManager = new ProductManager();
+  console.log(await productManager.getProducts());
+  await productManager.addProduct(
+    "producto prueba",
+    "Este es un producto prueba",
+    200,
+    "Sin imagen",
+    "abc123",
+    25
+  );
+  await productManager.getProducts();
+  await productManager.addProduct(
+    "producto prueba",
+    "Este es un producto prueba",
+    200,
+    "Sin imagen",
+    "abc123",
+    25
+  );
+  console.log(await productManager.getProducts());
+  await productManager.getProductsById(2);
+  await productManager.addProduct(
+    "producto prueba 2 ",
+    "Este es un producto prueba 23",
+    150,
+    "Sin imagen",
+    "abc1231",
+    25
+  );
+  await productManager.updateProducts(2, {
+    title: "producto prueba 25 ",
+    description: "Este es un producto prueba 23",
+    price: 150,
+    thumbnail: "Sin imagen",
+    code: "abc1231",
+    stock: 25,
+    id: 5,
+  });
+  await productManager.deleteProduct(2);
+};
 
-productManager.addProducts("productazo", "es un comestible", 200, "img not found", "200715", 10);
-productManager.getProducts();
-productManager.addProducts("productazo", "es un comestible", 200, "img not found", "200715", 10);
-productManager.getProducts();
-productManager.getProductsById(2)
-
+funcionAsync();
